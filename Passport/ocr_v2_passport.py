4b7b74183c173482944747c0789b8277f11b59f6
AshritaGogada <ashritagogada@gmail.com>
2025-01-18 22:17:16 +0530

    Update data.json and outputbase.txt with new passport details and formatting adjustments

--- Passport/ocr_v2_passport.py
+++ Passport/ocr_v2_passport.py
@@ -8,8 +8,6 @@ import re
 import io
 import json
 import ftfy
-# from nostril import nonsense
-
 
 ################################################################################################################
 ############################# Section 1: Initiate the command line interface ###################################
@@ -36,7 +34,6 @@ Our command line arguments are parsed. We have two command line arguments:
                 - gauss
                 - bilateral
                 - thresh (meadian threshold - default)
-                
 ---------------------------  Use Blur when the image has noise/grain/incident light etc. --------------------------
 '''
 
@@ -56,14 +53,7 @@ if args["preprocess"] == "thresh":
 
 elif args["preprocess"] == "adaptive":
     gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-'''
-What we would like to do is to add some additional preprocessing steps as in most cases, you may need to scale your 
-image to a larger size to recognize small characters. 
-In this case, INTER_CUBIC generally performs better than other alternatives, though it’s also slower than others.
 
-If you’d like to trade off some of your image quality for faster performance, 
-you may want to try INTER_LINEAR for enlarging images.
-'''
 if args["preprocess"] == "linear":
     gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
 
@@ -71,7 +61,6 @@ elif args["preprocess"] == "cubic":
     gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
 
 # make a check to see if blurring should be done to remove noise, first is default median blurring
-
 if args["preprocess"] == "blur":
     gray = cv2.medianBlur(gray, 3)
 
@@ -94,14 +83,7 @@ cv2.imwrite(filename, gray)
 # load the image as a PIL/Pillow image, apply OCR, and then delete
 # the temporary file
 text = pytesseract.image_to_string(Image.open(filename), lang = 'eng')
-# add +hin after eng within the same argument to extract hindi specific text - change encoding to utf-8 while writing
 os.remove(filename)
-# print(text)
-
-# show the output images
-# cv2.imshow("Image", image)
-# cv2.imshow("Output", gray)
-# cv2.waitKey(0)
 
 # writing extracted data into a text file
 text_output = open('outputbase.txt', 'w', encoding='utf-8')
@@ -110,17 +92,10 @@ text_output.close()
 
 file = open('outputbase.txt', 'r', encoding='utf-8')
 text = file.read()
-# print(text)
 
 # Cleaning all the gibberish text
 text = ftfy.fix_text(text)
 text = ftfy.fix_encoding(text)
-'''for god_damn in text:
-    if nonsense(god_damn):
-        text.remove(god_damn)
-    else:
-        print(text)'''
-# print(text)
 
 ############################################################################################################
 ###################################### Section 4: Extract relevant information #############################
@@ -146,20 +121,15 @@ for lin in lines:
     text1.append(s)
 
 text1 = list(filter(None, text1))
-# print(text1)
 
 # to remove any text read from the image file which lies before the line 'Income Tax Department'
-
 lineno = 0  # to start from the first line of the text file.
-
-# text1 = list(text1)
 text0 = text1[lineno+1:]
-print(text0)  # Contains all the relevant extracted text in form of a list - uncomment to check
 
 def findword(textlist, wordstring):
     lineno = -1
     for wordline in textlist:
-        xx = wordline.split( )
+        xx = wordline.split()
         if ([w for w in xx if re.search(wordstring, w)]):
             lineno = textlist.index(wordline)
             textlist = textlist[lineno+1:]
@@ -167,48 +137,39 @@ def findword(textlist, wordstring):
     return textlist
 
 ###############################################################################################################
-######################################### Section 5: Dishwasher part ##########################################
+######################################### Section 5: Extracting Data ##########################################
 ###############################################################################################################
 try:
+    # Extracting Surname
+    surname_line = [line for line in text0 if re.search(r'^[A-Z]+$', line)]
+    surname = surname_line[0] if surname_line else ""
+    surname = re.sub('[^a-zA-Z]+', ' ', surname)
 
-    # Cleaning Surname
-    surname = text0[3]
-    surname = surname.rstrip()
-    surname = surname.lstrip()
-    surname = re.sub('[^a-zA-Z] +', ' ', surname)
+    # Extracting First Name
+    first_name_line = [line for line in text0 if re.search(r'[A-Z]{2,}', line)]
+    first_name = first_name_line[1] if first_name_line else ""
+    first_name = re.sub('[^a-zA-Z]+', ' ', first_name)
 
-    # Cleaning First Name
-    first_name = text0[5]
-    first_name = first_name.rstrip()
-    first_name = first_name.lstrip()
-    first_name = re.sub('[^a-zA-Z] +', ' ', first_name)
+    # Extracting Date of Birth (DOB) and ensuring it's in dd/mm/yyyy format
+    dob_line = [line for line in text0 if re.search(r'\d{2}/\d{2}/\d{4}', line)]
+    dob = dob_line[0] if dob_line else ""
+    
+    # Extracting Gender (M/F)
+    gender_line = [line for line in text0 if re.search(r'[M|F]', line)]
+    gender = gender_line[0] if gender_line else "M"  # Assuming 'M' for male if not found
+    
+    # Extracting Passport Number
+    passport_line = [line for line in text0 if re.search(r'\b[A-Z]{2}\d{7}\b', line)]
+    number = passport_line[0] if passport_line else ""
+    
+    # Extracting Date of Expiry (DOE)
+    doe_line = [line for line in text0 if re.search(r'\d{2}/\d{2}/\d{4}', line)]
+    doe = doe_line[1] if doe_line and len(doe_line) > 1 else ""
 
-    # Cleaning DOB
-    dob = text0[7]
-    dob = dob.rstrip()
-    dob = dob.lstrip()
-    dob = dob[-12:]
+except Exception as e:
+    print(f"Error: {e}")
 
-    # Cleaning Gender
-    gender = text0[4]
-    gender = 'M' # need to fix this
-
-    # Cleaning Passport Number
-    number = text0[1]
-    number = number[-8:]
-    number = number.rstrip()
-    number = number.lstrip()
-
-    # Cleaning DOE
-    doe = text0[14]
-    doe = doe.rstrip()
-    doe = doe.lstrip()
-    doe = doe[-12:-2]
-
-except:
-    pass
-
-# Making tuples of data
+# Creating final data structure
 data = {}
 data['Surname'] = surname
 data['First Name'] = first_name
@@ -217,10 +178,8 @@ data['Gender'] = gender
 data['Number'] = number
 data['Date of Expiry'] = doe
 
-# print(data)
-
 ###############################################################################################################
-######################################### Section 6: Write Data to JSONs ######################################
+######################################### Section 6: Write Data to JSON ######################################
 ###############################################################################################################
 
 # Writing data into JSON
@@ -238,10 +197,9 @@ with io.open('data.json', 'w', encoding='utf-8') as outfile:
 with open('data.json', encoding = 'utf-8') as data_file:
     data_loaded = json.load(data_file)
 
-# print(data == data_loaded)
-
-# Reading data back JSON(give correct path where JSON is stored)
+# Reading data back from JSON
 with open('data.json', 'r', encoding= 'utf-8') as f:
     ndata = json.load(f)
 
-print(ndata)
+print(ndata)  # Printing the extracted and cleaned data
+
